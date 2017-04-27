bdd5d270e8895787566b3811dd64a58856553cc4
UHanczar <u.hanczar@gmail.com>
2017-04-27 13:50:58 +0300

    fixing_errors

--- src/index.test.js
+++ src/index.test.js
@@ -13,8 +13,8 @@ describe('index.html', () => {
   it('should say hello', (done) => {
     const index = fs.readFileSync('./src/index.html', 'utf-8');
     jsdom.env(index, (err, window) => {
-      const h1 = window.document.getElementsByTagName('h1');
-      expect(h1.innerHTML).to.equal('Hello, fellas');
+      const h1 = window.document.getElementsByTagName('h1')[0];
+      expect(h1.innerHTML).to.equal('Hello, world!');
       done();
       window.close;
     });
